7efd7e00daa4f0f510bb97a534ed554cae744b19
Cozma Rares <cozmarares11@gmail.com>
2025-02-27 14:53:49 +0200

    chore: modify initial config

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/** @type {import('prettier').Config} */
+const config = {
+  plugins: [],
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  singleAttributePerLine: true,
+};
+
+export default config;
